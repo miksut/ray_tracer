356376434f7b342e5dc8b4c4f254e4cfae67f4a5
Faruk Acibal <f_acibal@hotmail.com>
2019-12-15 23:07:24 +0100

    Add simple PPM image export

--- src/GLEmbreeTracer.cpp
+++ src/GLEmbreeTracer.cpp
@@ -189,8 +189,12 @@ namespace cgCourse
     embree_render.build_bvh();
 
     float * frame = embree_render.cast_rays(getWindowSize(), cam);
-    //TODO: Output frame to the disk
-    delete [] frame;
+	
+	//test image
+	float test[12] = { 0.1, 0.2, 0.1, 0.2, 0.3, 0.4, 0.9, 0.9, 0.9, 0.5, 0.0, 0.0 };
+	ImageSaver::saveImageAsPPM("test", 2, 2, test);
+
+	delete[] frame;
   }
 }
 
